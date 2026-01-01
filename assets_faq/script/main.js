0f058207becb60df86ce43961ec673675a6dc158
emmh81171-png <emmh81171@gmail.com>
2026-01-01 15:11:51 +0900

    260101_hiiro.hachiya

--- assets_faq/script/main.js
+++ assets_faq/script/main.js
@@ -10,17 +10,3 @@ $(window).on('resize', function () {
 
     hamburger.removeClass('hamburger-menu-active')
 });
-
-// アコーディオンのタイトルがクリックされたら
-$('.question__acordion').on('click', function (e) {
-    // .accordion-contentを選択
-    var content = $(this).next();
-
-    // .accordion-contentを表示・非表示
-    content.slideToggle();
-});
-
-
-$('.question__acordion').on('click', function () {
-    $(this).toggleClass('active');
-});
